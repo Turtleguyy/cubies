b9350c732b764ec9499929d2cbf0e50f5eecfc2c
Zachary Nicoll <tower@zdn.me>
2013-03-14 19:06:13 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cubies::Application.config.secret_token = 'c7e190499e3f7f4c06c3e1daf8d14e4f06de8d920ab97a85aa9505c37a5567f0204f548f3f46d331bc2f1310b8949dd157bf31349b550f5163f35d7f6f2d8b9d'
